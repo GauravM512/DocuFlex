c5f9641db374a5091d9b03285f8b6bcaa2dbde71
GauravM512 <gauravm5612@gmail.com>
2026-03-23 21:24:37 +0530

    Remove test.py file containing unused PDF processing code

--- test.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfWriter
-
-writer = PdfWriter(clone_from="example.pdf")
-
-for page in writer.pages:
-    for img in page.images:
-        img.replace(img.image, quality=10)
-
-writer.write("out-low-quality.pdf")
